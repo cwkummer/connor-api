1c111f006c9e8aa35f68ab0afbe1754e7a87b134
Chris Kummer <chris.w.kummer@gmail.com>
2018-06-08 12:21:49 -0700

    Break out register and reconnect, ttl on redis sessionIDs

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Promise = bluebird = require('bluebird')
-const uuid = require('uuid/v4');
 
 // Connect to redis
 const redis = require('redis')
@@ -16,10 +15,22 @@ const wss = new webSocket.Server({ port: 8989 })
 let webSockets = {}
 
 // Get globally unique sessionPIN
-const getSessionPIN = async () => {
-  do { sessionPIN = Math.floor(Math.random() * (100000)) }
-  while (await client.existsAsync(sessionPIN))
-  return sessionPIN
+const getSessionID = async () => {
+  do { sessionID = Math.floor(Math.random() * (100000)) }
+  while (await client.existsAsync(sessionID))
+  return sessionID
+}
+
+// Register a new client
+const registerClient = async (ws) => {
+  webSockets[ws.sessionID] = ws
+  let registerRecord = JSON.stringify({ sessionID: ws.sessionID, userID: "" })
+  let res = await client.setexAsync(ws.sessionID, 3600, registerRecord)
+  let message = JSON.stringify({
+    type: 'NEW_CLIENT',
+    sessionID: ws.sessionID
+  })
+  pub.publish('connor-global', message)
 }
 
 // Receive sub messages and send to target websocket if it exists
@@ -29,56 +40,60 @@ sub.on('message', (channel, msg) => {
 })
 
 wss.on('connection', async (ws) => {
-  // New websocket connection
-  let sessionPIN = await getSessionPIN() 
-  let sessionID = uuid()
   ws.isAlive = true
-  ws.sessionID = sessionID
-  ws.sessionPIN = sessionPIN
-  webSockets[sessionID] = ws
-  let registerRecord = JSON.stringify({ sessionID: sessionID, userID: "" })
-  let res = await client.setAsync(sessionPIN, registerRecord)
-  ws.send(JSON.stringify({
-    type: 'REGISTER_SUCCESS',
-    sessionPIN: sessionPIN,
-    sessionID: sessionID
-  }))
-  let message = JSON.stringify({
-    type: 'NEW_CLIENT',
-    sessionPIN: sessionPIN,
-    sessionID: sessionID
-  })
-  pub.publish('connor-global', message)
   // React to Pong events
   ws.on('pong', () => {
     ws.isAlive = true
-    console.log(`${ws.sessionID} is alive!`)
+    if (ws.sessionID) { client.expireAsync(sessionID, 3600) }
+    console.log(`${ws.sessionID} - alive`)
   })
   // Close websocket connection
-  ws.on('close', () => { delete webSockets[sessionID] })
+  ws.on('close', () => {
+    if (ws.sessionID) {
+      delete webSockets[ws.sessionID]
+      client.delAsync(ws.sessionID)
+    }
+    console.log(`${ws.sessionID} - closed`)
+  })
   // Message on existing websocket connection
   ws.on('message', async (message) => {
     const data = JSON.parse(message)
     switch (data.type) {
-      case 'REGISTER_ATTEMPT': {        
+      case 'REGISTER_ATTEMPT': {
+        ws.sessionID = await getSessionID() 
+        await registerClient(ws)
+        ws.send(JSON.stringify({
+          type: 'REGISTER_SUCCESS',
+          sessionID: ws.sessionID
+        }))
+        break
+      }
+      case 'RECONNECT_ATTEMPT': {
+        ws.sessionID = data.sessionID
+        await registerClient(ws)
+        ws.send(JSON.stringify({
+          type: 'RECONNECT_SUCCESS',
+          sessionID: ws.sessionID
+        }))
         break
       }
       default:
+        pub.publish('connor-global', message)
         break
     }
   })
 })
 
 // Ping all websockets every 10 seconds
-setInterval(() => {
+setInterval(async () => {
   console.log(Object.keys(webSockets).length)
   wss.clients.forEach((ws) => {
     if (!ws.isAlive) {
-      console.log(`${ws.sessionID} is dead!`)
+      console.log(`${ws.sessionID} - dead`)
       return ws.terminate()
     }
     ws.isAlive = false
-    console.log(`Pinging ${ws.sessionID}`)
+    console.log(`${ws.sessionID} - ping`)
     ws.ping(null, false, true)
   })
 }, 10000)
